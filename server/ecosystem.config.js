84422bb4afb377ec09a8658f06dd7b91f2715f45
Subhojit Paul <subhojitpaul.paul10@gmail.com>
2024-12-05 12:50:44 +0530

    server modidy

---
+++ server/ecosystem.config.js
@@ -0,0 +1,10 @@
+export const apps = [
+    {
+        name: "project-management",
+        script: "npm",
+        args: "run dev",
+        env: {
+            NODE_ENV: "development",
+        },
+    },
+];
